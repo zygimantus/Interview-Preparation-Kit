1afa1ad59c9877ace7feb9bd55dcd0c727cf445f
Zygimantus <zygimantus@gmail.com>
2020-03-01 11:50:40 +0200

    First commit

---
+++ src/main/java/com/zygimantus/hackerrank/ipk/App.java
@@ -0,0 +1,13 @@
+package com.zygimantus.hackerrank.ipk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
